4acff3c700edf1e3eae7eb78e5fddca44d1e8832
mihaivaidos <mihai@vaidos.com>
2025-05-10 17:07:35 +0300

    Created the IRepository interface and the InMemoryRepository class.

---
+++ SiemensDotNetProblem/LibraryRepository/InMemoryRepository.cs
@@ -0,0 +1,39 @@
+﻿namespace SiemensDotNetProblem.LibraryRepository;
+
+public class InMemoryRepository<T> : IRepository<T> where T : IHasID
+{
+    
+    private readonly Dictionary<int, T> _data = new();
+    
+    public void Add(T obj)
+    {
+        if (!_data.ContainsKey(obj.ID))
+        {
+            _data[obj.ID] = obj;
+        }
+    }
+
+    public void Update(T obj)
+    {
+        if (_data.ContainsKey(obj.ID))
+        {
+            _data[obj.ID] = obj;
+        }
+    }
+
+    public void Delete(int id)
+    {
+        _data.Remove(id);
+    }
+
+    public T Get(int id)
+    {
+        _data.TryGetValue(id, out var obj);
+        return obj!;
+    }
+
+    public List<T> GetAll()
+    {
+        return new List<T>(_data.Values);
+    }
+}
